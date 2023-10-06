3d84366df9b27a0705d1d87d35d73baba85d84f4
fatimabatooll <fatima.batool@xloopdigital.com>
2023-10-06 10:06:10 +0500

    modify

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
          <img src={logo} alt="sumz_logo" className='w-28 object-contain'/>
          <button
          type='button'
-         onClick={() => window.open("https://github.com/fatimabatooll")}
+         onClick={() => window.open("https://github.com/fatimanazeer")}
          className='black_btn'>
             Github
          </button>
